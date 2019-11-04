89db2c9edf488b197d31445405aa9a799ff3a524
Christian Wigert <christian.wigert@gmail.com>
2019-11-04 22:37:31 +0000

    added comments and some styling

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -55,12 +55,4 @@ def gram_params
   params.require(:gram).permit(:message, :picture)
 end
 
-def render_not_found
-  render plain: 'Not Found', status: :not_found
-end
-
-def render_not_found(status=:not_found)
-    render plain: "#{status.to_s.titleize} :(", status: status
-  end
-
 end
--- spec/helpers/grams_helper_spec.rb
+++ spec/helpers/grams_helper_spec.rb
@@ -1,5 +1,3 @@
 require 'rails_helper'
-
-RSpec.describe GramsHelper, type: :helper do
-
+  RSpec.describe GramsHelper, type: :helper do
 end
